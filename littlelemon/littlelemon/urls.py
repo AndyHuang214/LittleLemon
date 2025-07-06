f485fa33245950674ab8073c999745cc88a8d504
andy huang <ahuang@inter-state.com>
2025-07-06 14:38:49 -0400

    add booking API

--- littlelemon/littlelemon/urls.py
+++ littlelemon/littlelemon/urls.py
@@ -16,8 +16,18 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from rest_framework import routers
+from restaurant import views
+
+# urlpatterns = [
+#     path('admin/', admin.site.urls),
+#     path('restaurant/', include('restaurant.urls')),
+#     path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
+# ]
+
+router = routers.DefaultRouter()
+router.register(r'tables', views.BookingViewSet)
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('restaurant/', include('restaurant.urls')),
+    path('restaurant/booking/', include(router.urls)),
 ]
